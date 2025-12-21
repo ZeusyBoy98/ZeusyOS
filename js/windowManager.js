1cb8e1c922c05ca59f151c7fe8c20a1f9906d060
ZeusyBoy98 <wetcementstudios@gmail.com>
2025-12-21 13:15:42 +0800

    Draggable test window

---
+++ js/windowManager.js
@@ -0,0 +1,36 @@
+dragElement(document.getElementById("window"));
+
+function dragElement(element) {
+    var initialX = 0, initialY = 0, currentX = 0, currentY = 0;
+
+    if (document.getElementById(element.id + "header")) {
+        document.getElementById(element.id + "header").onmousedown = startDragging;
+    } else {
+        element.onmousedown = startDragging;
+    }
+
+    function startDragging(e) {
+        e = e || window.event;
+        e.preventDefault();
+        initialX = e.clientX;
+        initialY = e.clientY;
+        document.onmouseup = stopDragging;
+        document.onmousemove = dragElement;
+    }
+
+    function dragElement(e) {
+        e = e || window.event;
+        e.preventDefault();
+        currentX = initialX - e.clientX;
+        currentY = initialY - e.clientY;
+        initialX = e.clientX;
+        initialY = e.clientY;
+        element.style.top = (element.offsetTop - currentY) + "px";
+        element.style.left = (element.offsetLeft - currentX) + "px";
+    }
+
+    function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
