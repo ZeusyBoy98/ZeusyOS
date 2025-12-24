33a8a4a0d958a08466c4d2d24e3cc2828ec85f30
ZeusyBoy98 <wetcementstudios@gmail.com>
2025-12-24 20:05:45 +0800

    Started work on landslide browser

--- js/windowManager.js
+++ js/windowManager.js
@@ -14,6 +14,7 @@ const sketch = document.getElementById("sketch")
 
 /* --------Window Management-------- */
 initializeWindow(test);
+initializeWindow(landslide);
 initializeWindow(konami);
 initializeWindow(usernameChanger);
 initializeWindow(notepad);
