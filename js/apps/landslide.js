3972da36a894a785348f058b025e15f9784c9943
ZeusyBoy98 <wetcementstudios@gmail.com>
2025-12-27 10:33:24 +0800

    Finished blueit app

--- js/apps/landslide.js
+++ js/apps/landslide.js
@@ -1,25 +1,37 @@
-const buttons = [
-    document.getElementById("homeButton"),
-    document.getElementById("metoobButton"),
-    document.getElementById("forumButton"),
-    document.getElementById("docsButton"),
-];
+const searchBar = document.getElementById("landslideSearchBar");
 
 const pages = [
-    document.getElementById("landslideHome"),
-    document.getElementById("metoob"),
-    document.getElementById("forum"),
-    document.getElementById("docs"),
+    {
+        button: document.getElementById("homeButton"),
+        page: document.getElementById("landslideHome"),
+        label: "lctp://landslide.rocks"
+    },
+    {
+        button: document.getElementById("metoobButton"),
+        page: document.getElementById("metoob"),
+        label: "lctp://metoob.com"
+    },
+    {
+        button: document.getElementById("forumButton"),
+        page: document.getElementById("forum"),
+        label: "lctp://blueit.com"
+    },
+    {
+        button: document.getElementById("docsButton"),
+        page: document.getElementById("docs"),
+        label: "about:landslidedocs"
+    }
 ];
 
-buttons.forEach((button, i) => {
-    button.onclick = () => switchWindow(pages[i]);
+pages.forEach(({ button, page, label }) => {
+    button.onclick = () => switchWindow(page, label);
 });
 
-function switchWindow(activePage) {
-    pages.forEach(page => {
+function switchWindow(activePage, label) {
+    pages.forEach(({ page }) => {
         page.style.display = "none";
     });
 
     activePage.style.display = "flex";
+    searchBar.textContent = label;
 }
